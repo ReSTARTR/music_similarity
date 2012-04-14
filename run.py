ed948cf2ec45aa10510a4660beae0d8919668813
ReSTARTR <yoshida.masaki+restartr@gmail.com>
2012-04-15 02:03:48 +0900

    bug fix

--- run.py
+++ run.py
@@ -14,18 +14,41 @@ api = lastfm.Api(API_KEY)
 
 r = redis.Redis()
 
-def set_artist(artist):
-    for n in artist['similars']:
-        r.sadd('artist:sims:%s' % artist['name'], n)
+class Artist(object):
+    def __init__(self, name):
+        self._name = name
 
-    cnt = 1
-    with r.pipeline() as pipe:
-        for t in artist['tags']:
-            pipe.zincrby('rank:tags', t, 1)
-            pipe.sadd('tag:artists:%s' % t, artist['name'])
-            pipe.zadd('artist:tags:%s' % artist['name'], t, cnt)
-            pipe.execute()
-            cnt += 1
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def tags(self):
+        if not hasattr(self, '_tags'):
+            self._tags = r.lrange('artist:tags:%s' % self._name, 0, MATCH_TAGS)
+        return self._tags
+
+    @property
+    def similars(self):
+        if not hasattr(self, '_similars'):
+            similars = [ (name,score) for (name,score) in r.zrevrange('artist:similars:%s' % self._name, 0, MATCH_TAGS, withscores=True)]
+            similars = sorted(similars, key=lambda x:x[1], reverse=True)
+            self._similars = [ Artist(k) for (k,v) in sorted(similars, key=lambda x:x[1], reverse=True)]
+        return self._similars
+
+    def tags_intersect(self, artist, match_tags=None):
+        score = MATCH_TAGS
+        ts = []
+        match_tags = match_tags or MATCH_TAGS #int(MATCH_TAGS*0.25)
+        for t in self.tags[:match_tags]:
+            if t in artist.tags:
+                ts.append( (t, score) )
+            score -= 1
+        return ts
+
+    def similarity(self, artist):
+        intersect = self.tags_intersect(artist)
+        return gen_tag_score(intersect) if len(intersect) else 0.0
 
 def gen_tag_score(tags):
     x = sum([ e[1] for e in tags])
@@ -46,17 +69,26 @@ def make_similar_artist(name, targets):
 def load_artist_info(name):
     try:
         artist = api.get_artist(artist=name.decode('utf-8'))
-        if artist:
-            tags = [ tag.name.lower() for tag in artist.top_tags ]
-            similars =  [ s.name.encode('utf-8') for s in artist.similar ]
-            set_artist(
-                    {'name': name,
-                     'tags': tags,
-                     'similars': similars})
+        if not artist:
+            return
+        r.rpush('list:artists', artist.name)
+        tags = [ tag.name.lower() for tag in artist.top_tags ]
+        similars =  [ s.name.encode('utf-8') for s in artist.similar ]
+
+        with r.pipeline() as pipe:
+            for n in similars:
+                pipe.rpush('artist:sims:%s' % artist.name, n)
+            pipe.execute() 
+
+            for tag in tags:
+                pipe.zincrby('rank:tags', tag, 1)
+                pipe.sadd('tag:artists:%s' % tag, artist.name)
+                pipe.rpush('artist:tags:%s' % artist.name, tag)
+                pipe.execute()
     except lastfm.error.InvalidParametersError, e:
         print e
     finally:
-        time.sleep(1)
+        time.sleep(0.2)
 
 def get_artists():
     artists = []
@@ -66,10 +98,11 @@ def get_artists():
                 artists.append(line[:-1])
     return artists
 
-def make_similars(artists):
+def make_similars():
     r.delete('rank:artists')
     r.delete('artist:similars:*')
 
+    artists = get_artist_list()
     for artist_name in artists:
         anothers = [name for name in artists if name != artist_name]
         similars = make_similar_artist(artist_name, anothers)
@@ -82,12 +115,11 @@ def tag_score(tag):
     return r.zscore('tag:rank', tag) or 0
 
 def get_sims(artist):
-    return r.smembers('artist:sims:%s' % artist)
+    return r.lrange('artist:sims:%s' % artist, 0, -1)
 
 def get_same_tags(a, b):
-    a_tag = r.zrange('artist:tags:%s' % a, 1, MATCH_TAGS)
-    b_tag = r.zrange('artist:tags:%s' % b, 1, MATCH_TAGS)
-
+    a_tag = r.lrange('artist:tags:%s' % a, 1, MATCH_TAGS)
+    b_tag = r.lrange('artist:tags:%s' % b, 1, MATCH_TAGS)
     score = MATCH_TAGS
     same_tags = []
     for t in a_tag:
@@ -97,7 +129,7 @@ def get_same_tags(a, b):
     return same_tags
 
 def get_artist_tags(artist):
-    return r.zrange('artist:tags:%s' % artist, 0, -1)
+    return r.lrange('artist:tags:%s' % artist, 0, -1)
 
 def get_similar_artists(name):
     return r.zrevrange('artist:similars:%s' % name, 0, -1, withscores=True)
@@ -111,6 +143,8 @@ def get_top_artists():
 def get_similar_artists_in(name, artist_names):
     return [ s for s in get_sims(name) if s.upper() in artist_names ]
 
+def get_artist_list():
+    return r.lrange('list:artists', 0, -1)
 
 def update_db():
     r.flushdb()
@@ -123,26 +157,21 @@ def update_db():
         print cnt,'/',len(artists)
         load_artist_info(artist_name)
         cnt += 1
-    make_similars(artists)
+    make_similars()
 
 def read():
     artists = get_top_artists()
     artist_names = [ n for (n,s) in artists ]
     for (name,score) in artists:
-        similars = get_similar_artists(name)
-        print '----'
-        print name
+        artist = Artist(name)
+        print '\n###', artist.name, 'score:', score, '\n'
 
-        print 'similar from last.fm:', get_similar_artists_in(name, artist_names)
-
-        for (name_,similarity) in similars[:5]:
-            tags = get_same_tags(name, name_)
-            tags = sorted(tags, key=lambda x: x[1], reverse=True)
-            print '\t', similarity, '\tname:',name_,  '\ttags:','/'.join(dict(tags).keys())
-
-    print get_top_tags()[:10]
+        for a in artist.similars[:5]:
+            score = artist.similarity(a)
+            if score>0:
+                print ' ', '%3d' % int(score*10), a.name, artist.tags_intersect(a)[:4]
 
 if __name__ == '__main__':
-    #update_db()
+    update_db()
     read()
 
