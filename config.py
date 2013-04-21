a8269968691a26e3b1ee5f3dc9ce59a1c67f11df
ReSTARTR <yoshida.masaki+restartr@gmail.com>
2013-04-21 23:53:54 +0900

    Add frf2013 artists

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 settings={
+    # 2012
     'frf2012': {
         'desc': 'FUJIROCK FESTIVAL \'12',
         'path':'./resources/frf2012.txt',
@@ -30,6 +31,12 @@ settings={
         'desc': 'Rock in Japan 2012',
         'path': './resources/rockinjapan2012.txt',
         'db'  : 5,
+    },
+    # 2013
+    'frf2013': {
+        'desc': 'FUJIROCK FESTIVAL \'13',
+        'path':'./resources/frf2013.txt',
+        'db'  : 6,
     },
 }
 
